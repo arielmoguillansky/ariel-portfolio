6abe3b810fc5634fe0e91cf2661f9cc9cdd2b04e
arielmoguillansky <arielmogui92@gmail.com>
2026-07-16 09:46:57 -0300

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, Space_Grotesk, JetBrains_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 // Body / UI text
 const inter = Inter({
@@ -22,12 +23,15 @@ const jetbrainsMono = JetBrains_Mono({
   weight: ["400", "500"],
 });
 
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
       className={`${inter.className} ${inter.variable} ${spaceGrotesk.variable} ${jetbrainsMono.variable}`}
     >
       <Component {...pageProps} />
+      {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
     </div>
   );
 }
